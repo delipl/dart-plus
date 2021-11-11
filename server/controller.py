5f86bf98e7e69ca15a981319fd2a42f91279541a
Bartosz <kwiatkowsky.kontakt@gmail.com>
2021-11-11 22:26:58 +0100

    Add player table, and updating single entity

--- server/controller.py
+++ server/controller.py
@@ -1,6 +1,4 @@
-from database import get_db
-
-ID = 1  # at this moment we need one game
+from database import get_db, Setting, Player
 
 
 # Insert settings table, that was happend when game has been started.
@@ -13,31 +11,101 @@ def insert_settings(gameStatus, maxThrow, numberOfThrow, startTime, throwingPlay
     return True
 
 
-# Get all settings
-def get_settings():
+# Insert player
+def insert_player(name, nick, maxThrow, throws, average, wins, matches):
     db = get_db()
     cursor = db.cursor()
-    query = "SELECT id, gameStatus, maxThrow, numberOfThrow, startTime, throwingPlayerId, round FROM settings"
+    statement = "INSERT INTO players(name, nick, maxThrow, throws, average, wins, matches) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    cursor.execute(statement, [name, nick, maxThrow, throws, average, wins, matches])
+    db.commit()
+    return True
+
+
+# Get all settings
+def get_settings():
+    settings = []
+    db = get_db()
+    cursor = db.cursor()
+    query = "SELECT * FROM settings"
     cursor.execute(query)
-    return cursor.fetchall()
+    rows = cursor.fetchall()
+    for i in rows:
+        setting = Setting(i[0], i[1], i[2], i[3], i[4], i[5], i[6])
+        settings.append(setting.getDictionary())
+    return settings
+
+
+# Get all players
+def get_players():
+    players = []
+    db = get_db()
+    cursor = db.cursor()
+    query = "SELECT * FROM players"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for i in rows:
+        player = Player(i[0], i[1], i[2], i[3], i[4], i[5], i[6], i[7])
+        players.append(player.getDictionary())
+    return players
 
 
 # Update settings values
-def update_settings(gameStatus, maxThrow, numberOfThrow, startTime, throwingPlayerId, round):
+def update_settings(id, gameStatus, maxThrow, numberOfThrow, startTime, throwingPlayerId, round):
     db = get_db()
     cursor = db.cursor()
     statement = "UPDATE settings SET gameStatus = ?, maxThrow = ?, numberOfThrow = ?, " \
-                "startTime = ?, throwingPlayerId = ?, round = ? WHERE ID = ?"
-    cursor.execute(statement, [gameStatus, maxThrow, numberOfThrow, startTime, throwingPlayerId, round])
+                "startTime = ?, throwingPlayerId = ?, round = ? WHERE id = ?"
+    cursor.execute(statement, [gameStatus, maxThrow, numberOfThrow, startTime, throwingPlayerId, round, id])
+    db.commit()
+    return True
+
+
+# Update players values
+def update_players(id, name, nick, maxThrow, throws, average, wins, matches):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "UPDATE players SET name = ?, nick = ?, maxThrow = ?, " \
+                "throws = ?, average = ?, wins = ?, matches = ? WHERE id = ?"
+    cursor.execute(statement, [name, nick, maxThrow, throws, average, wins, matches, id])
     db.commit()
     return True
 
 
 # Delete setting from database
+def delete_setting(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "DELETE FROM settings WHERE id = ?"
+    cursor.execute(statement, [id])
+    db.commit()
+    return True
+
+
+# Delete player from database
+def delete_player(id):
+    db = get_db()
+    cursor = db.cursor()
+    statement = "DELETE FROM players WHERE id = ?"
+    cursor.execute(statement, [id])
+    db.commit()
+    return True
+
+
+# Delete all setting
 def delete_settings():
     db = get_db()
     cursor = db.cursor()
-    statement = "DELETE FROM settings WHERE ID = ?"
-    cursor.execute(statement, [ID])
+    statement = "DELETE FROM settings"
+    cursor.execute(statement)
+    db.commit()
+    return True
+
+
+# Delete all players
+def delete_players():
+    db = get_db()
+    cursor = db.cursor()
+    statement = "DELETE FROM players"
+    cursor.execute(statement)
     db.commit()
     return True
--- server/database.py
+++ server/database.py
@@ -3,6 +3,52 @@ import sqlite3
 DATABASE_NAME = 'database.db'
 
 
+class Setting:
+    def __init__(self, id, gameStatus, maxThrow, numberOfThrow, startTime, throwingPlayerId, round):
+        self.id = id
+        self.gameStatus = gameStatus
+        self.maxThrow = maxThrow
+        self.numberOfThrow = numberOfThrow
+        self.startTime = startTime
+        self.throwingPlayerId = throwingPlayerId
+        self.round = round
+
+    def getDictionary(self):
+        return {
+            "id": self.id,
+            "gameStatus": self.gameStatus,
+            "maxThrow": self.maxThrow,
+            "numberOfThrow": self.numberOfThrow,
+            "startTime": self.startTime,
+            "throwingPlayerId": self.throwingPlayerId,
+            "round": self.round
+        }
+
+
+class Player:
+    def __init__(self, id, name, nick, maxThrow, throws, average, wins, matches):
+        self.id = id
+        self.name = name
+        self.nick = nick
+        self.maxThrow = maxThrow
+        self.throws = throws
+        self.average = average
+        self.wins = wins
+        self.matches = matches
+
+    def getDictionary(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "nick": self.nick,
+            "maxThrow": self.maxThrow,
+            "throws": self.throws,
+            "average": self.average,
+            "wins": self.wins,
+            "matches": self.matches,
+        }
+
+
 def get_db():
     conn = sqlite3.connect(DATABASE_NAME)
     return conn
@@ -18,6 +64,17 @@ def create_tables():
                 startTime TEXT NOT NULL,
                 throwingPlayerId INTEGER NOT NULL,
                 round INTEGER NOT NULL
+            )
+            """,
+            """CREATE TABLE IF NOT EXISTS players(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                nick TEXT NOT NULL,
+                maxThrow INTEGER NOT NULL,
+                throws INTEGER NOT NULL,
+                average FLOAT NOT NULL,
+                wins INTEGER NOT NULL,
+                matches INTEGER NOT NULL
             )
             """
     ]
