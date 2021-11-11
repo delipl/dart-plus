7c1b1cd70bc078adcca4986168c05c53bd8e6a82
Bartosz <kwiatkowsky.kontakt@gmail.com>
2021-11-11 17:52:28 +0100

    Create and configure simple table in database

---
+++ server/database.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+DATABASE_NAME = 'database.db'
+
+
+def get_db():
+    conn = sqlite3.connect(DATABASE_NAME)
+    return conn
+
+
+def create_tables():
+    tables = [
+        """CREATE TABLE IF NOT EXISTS game(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                gameStatus BOOLEAN NOT NULL,
+                maxThrow INTEGER NOT NULL,
+                numberOfThrow INTEGER NOT NULL,
+                startTime TEXT NOT NULL,
+                throwingPlayerId INTEGER NOT NULL,
+                round INTEGER NOT NULL
+            )
+            """
+    ]
+    db = get_db()
+    cursor = db.cursor()
+    for table in tables:
+        cursor.execute(table)
