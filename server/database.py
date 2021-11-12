b5545f651f4a8404e5b76b653b5da48769607a90
Bartosz <kwiatkowsky.kontakt@gmail.com>
2021-11-12 03:32:32 +0100

    Create extra tables for throw,players, users. Add more details and some functionality

--- server/database.py
+++ server/database.py
@@ -1,17 +1,46 @@
 import sqlite3
+from datetime import datetime
 
 DATABASE_NAME = 'database.db'
 
 
-class Setting:
-    def __init__(self, id, gameStatus, maxThrow, numberOfThrow, startTime, throwingPlayerId, round):
+class Settings:
+    def __init__(self, amountOfUsers=2, startPoints=301, doubleIn=False, doubleOut=False):
+        self.amountOfUsers = amountOfUsers
+        self.startPoints = startPoints
+        self.doubleIn = doubleIn
+        self.doubleOut = doubleOut
+
+    def getDictionary(self):
+        return {
+            "amountOfUsers": self.amountOfUsers,
+            "startPoints": self.startPoints,
+            "doubleIn": self.doubleIn,
+            "doubleOut": self.doubleOut
+        }
+
+
+class Game:
+    def __init__(self, id, gameStatus, maxThrow, numberOfThrow, startTime, throwingUserId, round, setting, players: list):
         self.id = id
         self.gameStatus = gameStatus
         self.maxThrow = maxThrow
         self.numberOfThrow = numberOfThrow
         self.startTime = startTime
-        self.throwingPlayerId = throwingPlayerId
+        self.throwingUserId = throwingUserId
         self.round = round
+        self.setting = setting
+        self.players = []
+        for p in range(setting.amountOfUsers - 1):
+            self.players.append(Player(p, "Test" + str(p), setting.startPoints))
+
+        # test throw
+        pTest = Player(setting.amountOfUsers - 1, "TestThrow", setting.startPoints)
+        pTest.addThrow(3, 20)
+        pTest.addThrow(1, 17)
+        pTest.addThrow(3, 2)
+        pTest.calculateAverage() # test
+        self.players.append(pTest)
 
     def getDictionary(self):
         return {
@@ -20,12 +49,14 @@ class Setting:
             "maxThrow": self.maxThrow,
             "numberOfThrow": self.numberOfThrow,
             "startTime": self.startTime,
-            "throwingPlayerId": self.throwingPlayerId,
-            "round": self.round
+            "throwingUserId": self.throwingUserId,
+            "round": self.round,
+            "setting": self.setting.getDictionary(),
+            "players": getDictionary(self.players)
         }
 
 
-class Player:
+class User:
     def __init__(self, id, name, nick, maxThrow, throws, average, wins, matches):
         self.id = id
         self.name = name
@@ -49,6 +80,64 @@ class Player:
         }
 
 
+class Player:
+    def __init__(self, id, nick, points):
+        self.id = id
+        self.nick = nick
+        self.points = points
+        self.throws = []
+
+    # test
+    def calculateAverage(self):
+        sum = 0
+        for i in self.throws:
+            sum = sum + i.multiplier * i.points
+        print("Player " + str(self.id) + " average: " + str(round(sum / len(self.throws), 2)))
+
+    def addThrow(self, multiplier, points):
+        self.points = self.points - multiplier*points
+        self.throws.append(Throw(multiplier, points))
+
+    def removeLastThrow(self):
+        self.removeThrow(len(self.throws)-1)
+
+    def removeThrow(self, i):
+        self.points = self.points + self.throws[i].multiplier * self.throws[i].points
+        self.throws[i].remove(i)
+
+    def getDictionary(self):
+        return {
+            "id": self.id,
+            "nick": self.nick,
+            "points": self.points,
+            "throws": getDictionary(self.throws)
+        }
+
+
+def getDictionary(objects: list):
+    dict = []
+    for i in objects:
+        dict.append(i.getDictionary())
+    return dict
+
+
+class Throw:
+    def __init__(self, multiplier, points):
+        self.multiplier = multiplier
+        self.points = points
+        self.date = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+    def getScore(self):
+        return self.multiplier * self.points
+
+    def getDictionary(self):
+        return {
+            "multiplier": self.multiplier,
+            "points": self.points,
+            "date": self.date
+        }
+
+
 def get_db():
     conn = sqlite3.connect(DATABASE_NAME)
     return conn
@@ -56,17 +145,19 @@ def get_db():
 
 def create_tables():
     tables = [
-        """CREATE TABLE IF NOT EXISTS settings(
+        """CREATE TABLE IF NOT EXISTS games(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 gameStatus BOOLEAN NOT NULL,
                 maxThrow INTEGER NOT NULL,
                 numberOfThrow INTEGER NOT NULL,
                 startTime TEXT NOT NULL,
-                throwingPlayerId INTEGER NOT NULL,
-                round INTEGER NOT NULL
+                throwingUserId INTEGER NOT NULL,
+                round INTEGER NOT NULL,
+                setting pickle NOT NULL,
+                players pickle NOT NULL
             )
             """,
-            """CREATE TABLE IF NOT EXISTS players(
+        """CREATE TABLE IF NOT EXISTS users(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 name TEXT NOT NULL,
                 nick TEXT NOT NULL,
