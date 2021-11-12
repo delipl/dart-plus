3faf99263f8136e7c691639935d3c5976f8b9c4b
Bartosz <kwiatkowsky.kontakt@gmail.com>
2021-11-12 16:43:21 +0100

    Moved project to new directory and rework template

---
+++ app/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Dart = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>
+        Hello Dart-Plus! 🎉
+      </Text>
+    </View>
+  );
+}
+
+export default Dart;
