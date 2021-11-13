c435f88fe62e74483e72347e109672441024f9e4
Bartosz <kwiatkowsky.kontakt@gmail.com>
2021-11-13 17:26:39 +0100

    Create basic screen view with including http get method

--- app/src/screens/Dashboard.js
+++ app/src/screens/Dashboard.js
@@ -1,30 +1,70 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react';
+import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 import Background from '../components/Background'
 import Logo from '../components/Logo'
+import DartLogo from '../components/DartLogo'
 import Header from '../components/Header'
+import BHeader from '../components/BigHeader'
 import Paragraph from '../components/Paragraph'
+import BParagraph from '../components/BigParagraph'
 import Button from '../components/Button'
 
-export default function Dashboard({ navigation }) {
+function Images(props) {
+  const k = props.attempts
+  let render = []
+  for (let i = 0; i < k; i++) {
+    render.push(<DartLogo></DartLogo>)
+  }
+  return render
+}
+
+export default function Dashboard({ navigation }){
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  const getGame = async () => {
+     try {
+      const response = await fetch('http://192.168.192.3:8000/info', {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',    
+          'Content-Type': 'application/json'
+        }
+      });
+      const json = await response.json();
+      setData(json);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+  useEffect(() => {
+    getGame();
+  }, []);
+
+
+
   return (
     <Background>
-      {/* <Logo /> */}
-      <Header>Let’s start</Header>
+      <BHeader>
+        {data.points}
+      </BHeader>
+      <BParagraph>
+        {data.nick}
+      </BParagraph>
+      <Header>
+      <Images attempts={data.attempts}/>
+      </Header>
       <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
+      {/* <FlatList
+          data={data.players}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => (
+            <Paragraph>{item.nick} - {item.points} </Paragraph>
+          )}
+        /> */}
       </Paragraph>
-      <Button
-        mode="outlined"
-        onPress={() =>
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'StartScreen' }],
-          })
-        }
-      >
-        Logout
-      </Button>
     </Background>
-  )
+  );
 }
