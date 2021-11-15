aace837de9caed1a06deb098f8d835a0ab3a4942
Bartosz <kwiatkowsky.kontakt@gmail.com>
2021-11-15 02:33:26 +0100

    Fix function generate http response

--- server/messages.py
+++ server/messages.py
@@ -7,15 +7,7 @@ INTERNAL_SERVER_ERROR = 500
 OK = 200
 
 
-def generate_http_response(status, message=None, code=NOT_ACCEPTABLE):
-    if status:
-        status = 0
-        message = MESSAGE_OK
-    elif not status:
-        status = 1
-        message = MESSAGE_ERROR
-    if status == 0:
-        code = OK
+def generate_http_response(status, message=MESSAGE_ERROR, code=NOT_ACCEPTABLE):
     dictionary = {"status": status, "message": message}
     return jsonify(dictionary), code
 
