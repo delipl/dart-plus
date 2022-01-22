89c6faf3c1793af69441db65381e32050945a13f
Artur Ziółkowski <aaartiii99@gmail.com>
2022-01-22 17:53:11 +0100

    first unit tests to user api

--- server/app/models/user.py
+++ server/app/models/user.py
@@ -1,5 +1,6 @@
 import datetime
 
+from flask import current_app
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
@@ -15,7 +16,7 @@ user_game = db.Table('user_game',
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
-    password = db.Column(db.String(128))
+    password_hash = db.Column(db.String(128))
     name = db.Column(db.String(64), unique=True, index=True)
     nick = db.Column(db.String(64), unique=True, index=True)
     phone = db.Column(db.Integer, unique=True)
@@ -33,16 +34,16 @@ class User(db.Model):
     attempts = db.Column(db.Integer)
     points = db.Column(db.Integer)
 
-    # @property
-    # def password(self):
-    #     raise AttributeError('Cannot read password attribute')
+    @property
+    def password(self):
+        raise AttributeError('Cannot read password attribute')
 
-    # @password.setter
-    # def password(self, password):
-    #     self.password_hash = generate_password_hash(password)
-    #
-    # def verify_password(self, password):
-    #     return check_password_hash(self.password_hash, password)
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def to_json(self):
         games_ids = [game.id for game in self.active_games]
@@ -50,7 +51,7 @@ class User(db.Model):
             "id": self.id,
             "name": self.name,
             "phone": self.phone,
-            "password": self.password,
+            "password": self.password_hash,
             "nick": self.nick,
             "wins": self.wins,
             "throws": [throw.getScore() for throw in self.throws],
---
+++ server/tests/test_user_api.py
@@ -0,0 +1,116 @@
+import json
+import unittest
+
+from app import create_app, db
+from app.models.user import User, user_game
+from app.models.game import Game
+from app.models.dart_board import DartBoard
+
+
+class GameApiTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        # Role.insert_roles()
+        self.client = self.app.test_client(use_cookies=True)
+
+        dartBoard = DartBoard()
+        db.session.add(dartBoard)
+        artur = User(name='Artur', phone='123456780', password='huj', nick='louda', wins='2137', board=dartBoard)
+        bartek = User(name='Bartek', phone='123456789', password='huj', nick='la', wins='69', board=dartBoard)
+        kuba = User(name='Kuba', phone='123456788', password='huj', nick='uda', wins='420', board=dartBoard)
+        db.session.add(artur)
+        db.session.add(bartek)
+        db.session.add(kuba)
+        game = Game(startPoints='301')
+        db.session.add(game)
+        artur.active_games.append(game)
+        bartek.active_games.append(game)
+        db.session.commit()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_get_users(self):
+        response = self.client.get('/users/')
+        self.assertEqual(response.status_code, 200)
+        print(response.get_data(as_text=True))
+        # self.assertEqual(response.get_data(as_text=True),
+        #                 '{\"users\":[{\"attempts\":null,\"games_id\":[1],\"id\":1,\"name\":\"Artur\",\"nick\":\"louda\",\"password\":\"pbkdf2:sha256:150000$lfpJBpY9$baa59aa09ce609e2c7bfc4042ca17118581c698946a0428734fe298c05af6f8c\",\"phone\":123456780,\"throws\":[],\"wins\":2137},{\"attempts\":null,\"games_id\":[1],\"id\":2,\"name\":\"Bartek\",\"nick\":\"la\",\"password\":\"pbkdf2:sha256:150000$92nvX7tq$21d06a0f58eb2fa1d0317a348f8d8e41e4fef50b6525e62b5f39eda7b376c47a\",\"phone\":123456789,\"throws\":[],\"wins\":69},{\"attempts\":null,\"games_id\":[],\"id\":3,\"name\":\"Kuba\",\"nick\":\"uda\",\"password\":\"pbkdf2:sha256:150000$VbsIm207$018ff98917644f1b723a23e0db1552fbbc297464cbf3e01907e0987fdad293eb\",\"phone\":123456788,\"throws\":[],\"wins\":420}]}')
+
+    # def test_delete_games(self):
+    #     self.assertTrue(len(Game.query.all()) != 0)
+    #     response = self.client.delete('/games/1')
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertTrue(len(Game.query.all()) == 0)
+    #
+    # def test_get_game_by_id(self):
+    #     response = self.client.get('/games/1')
+    #     game = Game.query.first()
+    #     self.assertEqual(response.status_code, 200)
+    #     self.assertEqual(response.get_data(as_text=True),
+    #                     '{\"doubleIn\":false,\"doubleOut\":false,\"gameStatus\":null,\"id\":1,\"round\":null,\"startPoints\":301,\"startTime\":' + "\"" + game.startTime.strftime("%m/%d/%Y, %H:%M:%S") + "\"" + ',\"throwingUserId\":null,\"users_id\":[1,2]}\n')
+    #
+    # def test_create_new_game(self):
+    #     data = json.dumps(
+    #         {
+    #             "startPoints": 301,
+    #             "doubleOut": False,
+    #             "doubleIn": True,
+    #             "playersId": [1, 2]
+    #         })
+    #
+    #     headers = {'Content-Type': 'application/json'}
+    #
+    #     response = self.client.post('/games/', headers=headers, data=data)
+    #     self.assertEqual(response.status_code, 200)
+    #     game = Game.query.get_or_404(2)
+    #     self.assertEqual(game.startPoints, 301)
+    #     self.assertEqual([player.id for player in game.players], [1, 2])
+    #     self.assertEqual(game.doubleIn, True)
+    #     self.assertEqual(game.doubleOut, False)
+    #
+    # def test_update_game(self):
+    #     data = json.dumps(
+    #         {
+    #             "id": 1,
+    #             "status": 1,
+    #             "round": 1,
+    #             "value": 10,
+    #             "multiplier": 2,
+    #             "throwingPlayerId": 1,
+    #             "throwingUserId": 1,
+    #             "players": [
+    #                 {
+    #                     "id": 1,
+    #                     "attempts": 1,
+    #                     "points": 229
+    #                 },
+    #                 {
+    #                     "id": 2,
+    #                     "attempts": 5,
+    #                     "points": 259
+    #                 }
+    #             ]
+    #         })
+    #
+    #     headers = {'Content-Type': 'application/json'}
+    #     response = self.client.put('/games/', headers=headers, data=data)
+    #     self.assertEqual(response.status_code, 200)
+    #     game = Game.query.get_or_404(1)
+    #     player_1 = User.query.get_or_404(1)
+    #     player_2 = User.query.get_or_404(2)
+    #     self.assertEqual(game.startPoints, 301)
+    #     self.assertEqual(player_1.throws[0].value, 10)
+    #     self.assertEqual(player_1.throws[0].multiplier, 2)
+    #     self.assertEqual(player_1.points, 229)
+    #     self.assertEqual(player_2.points, 259)
+    #
+    #     response = self.client.put('/games/', headers=headers, data=data)
+    #     self.assertEqual(player_1.throws[1].value, 10)
+    #     self.assertEqual(player_1.throws[1].multiplier, 2)
+
